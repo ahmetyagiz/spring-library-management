b0d455fcbc2eed562ea7f32f7d9b76f4c618a766
ahmetyagiz <ahmetanteryagiz@gmail.com>
2025-02-20 18:39:38 +0300

    Initial commit: Added spring project

---
+++ src/main/java/com/ahmetyagiz/spring_library_management/SpringLibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ahmetyagiz.spring_library_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLibraryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahmetyagiz/spring_library_management/SpringLibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahmetyagiz.spring_library_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
